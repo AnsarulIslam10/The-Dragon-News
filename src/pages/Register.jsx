b7bc4117c3a32d694342220d749d0ed944654074
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-14 21:59:02 +0600

    create user with email and pass

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+    const {createNewUser, setUser} = useContext(AuthContext)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const form = new FormData(e.target)
+    const name = form.get("name")
+    const photo = form.get("photo")
+    const email = form.get("email")
+    const password = form.get("password")
+    console.log({name, photo, email, password})
+
+    createNewUser(email, password)
+    .then(result =>{
+        const user = result.user
+        console.log(user)
+        setUser(user)
+    }) .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+      });
+  };
+
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
         <h2 className="text-2xl font-semibold text-center">
           Register your account
         </h2>
-        <form className="card-body">
+        <form onSubmit={handleSubmit} className="card-body">
           <div className="form-control">
             <label className="label">
               <span className="label-text">Your Name</span>
             </label>
             <input
               type="text"
+              name="name"
               placeholder="Enter your name"
               className="input input-bordered"
               required
@@ -26,6 +51,7 @@ const Register = () => {
             </label>
             <input
               type="text"
+              name="photo"
               placeholder="Enter your photo url"
               className="input input-bordered"
               required
@@ -37,6 +63,7 @@ const Register = () => {
             </label>
             <input
               type="email"
+              name="email"
               placeholder="email"
               className="input input-bordered"
               required
@@ -48,6 +75,7 @@ const Register = () => {
             </label>
             <input
               type="password"
+              name="password"
               placeholder="password"
               className="input input-bordered"
               required
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,36 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.init";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  console.log(user);
+  const createNewUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const authInfo = {
+    user,
+    setUser,
+    createNewUser,
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
